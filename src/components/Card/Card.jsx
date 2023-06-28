36ff207ee7bfd571cfa13cf86211c9fd183d73af
Gustavo <User@SUB-MSOCIAL-VID>
2023-06-28 18:01:00 -0300

    searchbar added

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,7 +6,6 @@ export default function Card(props) {
       <div className={style.card}>
         <header className={style.card_header}>
           <p className={style.description}>{props.status}</p>
-
           <a href="#">
             <div className={style.closeContainer}>
               <div className={style.leftright}></div>
@@ -15,16 +14,12 @@ export default function Card(props) {
             </div>
           </a>
         </header>
-
         <div className={style.card_gallery}>
           <img src={props.image} alt={props.name} className={style.imgRM} />
         </div>
-
         <div className={style.card_body}>
           <h2 className={style.card_title}>{props.name}</h2>
           <div className={style.card_description}>
-
-            
 
             <p className={`${style.description} ${style.footerText}`}>{props.species}</p>
             <p className={`${style.description} ${style.footerText}`}>{props.gender}</p>
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,8 @@
 export default function SearchBar(props) {
    return (
       <div>
-         {/* <input type='search' />
-         <button onClick={}>Agregar</button> */}
+         <input type='search' />
+         <button onClick={() => window.alert(props.characterID)}>Agregar</button>
       </div>
    );
 }
